ab6be16dc5f30829106c55f4fcad622c1d10c68d
Bryan Boreham <bjboreham@gmail.com>
2015-06-04 15:01:14 +0100

    Tighten up notion of a blank CIDR to exclude 0.0.0.0/something

--- ipam/address/address.go
+++ ipam/address/address.go
@@ -40,7 +40,7 @@ func ParseCIDR(s string) (Address, CIDR, error) {
 
 func (cidr CIDR) Size() Offset { return 1 << uint(32-cidr.PrefixLen) }
 func (cidr CIDR) End() Address { return Add(cidr.Start, cidr.Size()) }
-func (cidr CIDR) Blank() bool  { return cidr.Start == 0 }
+func (cidr CIDR) Blank() bool  { return cidr.Start == 0 && cidr.PrefixLen == 0 }
 
 func (cidr CIDR) String() string {
 	return fmt.Sprintf("%s/%d", cidr.Start.String(), cidr.PrefixLen)
