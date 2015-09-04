bdbf110f4e8213395c98b3340f176a22f0e5ec0b
Bryan Boreham <bjboreham@gmail.com>
2015-09-04 11:51:43 +0100

    Run the test that might take a long time inside 'timeout'

--- test/640_proxy_restart_reattaches_test.sh
+++ test/640_proxy_restart_reattaches_test.sh
@@ -10,6 +10,15 @@ check_attached() {
 }
 
 N=50
+# Create and remove a lot of containers in a small subnet; the failure
+# mode is that this takes a long time as it has to wait for the old
+# ones to time out, so we run this function inside 'timeout'
+run_many() {
+    for i in $(seq $N); do
+        proxy docker_on $HOST1 run -e WEAVE_CIDR=net:10.32.4.0/28 --rm -t $SMALL_IMAGE /bin/true
+    done
+}
+
 start_suite "Proxy restart reattaches networking to containers"
 
 weave_on $HOST1 launch
@@ -37,10 +46,6 @@ sleep 1
 weave_on $HOST1 launch
 check_attached
 
-# Create and remove a lot of containers; the failure mode is that this
-# takes a long time as it has to wait for the old ones to time out.
-for i in $(seq $N); do
-    proxy docker_on $HOST1 run -e WEAVE_CIDR=net:10.32.4.0/28 --rm -t $SMALL_IMAGE /bin/true
-done
+assert_raises "timeout $N cat <( run_many )"
 
 end_suite
